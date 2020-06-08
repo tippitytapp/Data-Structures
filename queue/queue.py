f647fd91d1941a8b699c0732d7bbb58bbdd36b59
Marc Tapp <marc.tapp@gmail.com>
2020-06-08 13:16:18 -0400

    got the stack with arrys

--- queue/queue.py
+++ queue/queue.py
@@ -13,16 +13,53 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+
+# This implementation uses the array method as the underlying storage structure
+# class Queue:
+#     def __init__(self):
+#         self.queue = list()
+#         self.size = 0
+    
+#     def __len__(self):
+#         self.size = len(self.queue)
+#         return self.size
+
+#     def enqueue(self, data):
+#         self.queue.insert(0,data)
+
+#     def dequeue(self):
+#         if len(self.queue) == 0:
+#             return None
+#         value = self.queue.pop()
+#         return value
+        
+
+# This implementation uses the inked list implementation
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
 class Queue:
     def __init__(self):
-        self.size = 0
-        # self.storage = ?
+        self.head = None
+        self.last = None
     
-    def __len__(self):
-        pass
+    # def __len__(self):
+    #     length = len(self.data)
 
     def enqueue(self, value):
-        pass
+        if self.last is None:
+            self.head = Node(value)
+            self.last = self.head
+        else:
+            self.last.next = Node(value)
+            self.last = self.last.next
 
     def dequeue(self):
-        pass
+        if self.head is None:
+            return None
+        else:
+            data = self.head.data
+            self.head = self.head.next
+            return data
--- stack/stack.py
+++ stack/stack.py
@@ -13,13 +13,17 @@ return elements in Last In First Out order.
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = []
 
     def __len__(self):
-        pass
+        return len(self.storage)
 
     def push(self, value):
-        pass
+        self.storage.insert(0, value)
 
     def pop(self):
-        pass
+        if len(self.storage) > 0:
+            value = self.storage[0]
+            self.storage.pop(0)
+            return value
+        
